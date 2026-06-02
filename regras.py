a1f66cc075da257ec32882d1deb98f6b18eb6944
hollow <velpointl2@gmail.com>
2026-06-02 18:33:48 -0300

    refactor: divisão em módulos separados

--- regras.py
+++ regras.py
@@ -8,7 +8,7 @@ class RegraValidacao(ABC):
 
 class ValidacaoLimiteSimples(RegraValidacao):
     def validar(self, valor: float, regras: Dict[str, float]) -> str:
-        if (valor < regras["critico_min"]) or (valor > regras["critico_max"]):
+        if valor < regras["critico_min"] or valor > regras["critico_max"]:
             return 'Crítico'
         if regras["alerta_min"] <= valor <= regras["alerta_max"]:
             return 'Alerta'
